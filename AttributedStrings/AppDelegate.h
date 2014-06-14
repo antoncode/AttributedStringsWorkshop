b2c72e42a47236bbbcad04def086b51fd9ecbc07
Anton Rivera <anton.rivera@gmail.com>
2014-06-14 16:18:50 -0700

    initial commit

---
+++ AttributedStrings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AttributedStrings
+//
+//  Created by Anton Rivera on 6/14/14.
+//  Copyright (c) 2014 Anton Hilario Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AttributedStrings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttributedStrings
+//
+//  Created by Anton Rivera on 6/14/14.
+//  Copyright (c) 2014 Anton Hilario Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
