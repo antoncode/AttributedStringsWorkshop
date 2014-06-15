c7282d32e1fd9356fcc15fdfe8dac8d182611d80
Anton Rivera <anton.rivera@gmail.com>
2014-06-14 22:34:50 -0700

    clean up code by using titleAttributes property and getter method

--- AttributedStrings/DetailViewController.m
+++ AttributedStrings/DetailViewController.m
@@ -12,6 +12,7 @@
 
 @property (strong, nonatomic) IBOutlet UILabel *label;
 @property (strong, nonatomic) IBOutlet UITextView *textView;
+@property (strong, nonatomic) NSDictionary *titleAttributes;
 
 @end
 
@@ -21,28 +22,51 @@
 {
     [super viewDidLoad];
 
-    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:@"Speech"];
+//    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:@"Speech"];
     
-    UIFont *font = [UIFont fontWithName:@"DamascusBold" size:48.0f];
-    NSRange rangeOfTitle = NSMakeRange(0, [title.string length]);
+    NSAttributedString *title = [[NSAttributedString alloc] initWithString:@"Speech" attributes:_titleAttributes];
+    
+//    UIFont *font = [UIFont fontWithName:@"DamascusBold" size:48.0f];
+//    NSRange rangeOfTitle = NSMakeRange(0, [title.string length]);
+    
 //    [title addAttribute:NSFontAttributeName value:font range:rangeOfTitle];
 //    [title addAttribute:NSStrokeWidthAttributeName value:@2.0f range:rangeOfTitle];
     
-    NSShadow *shadow = [NSShadow new];
-    shadow.shadowBlurRadius = 2.0f;
-    shadow.shadowColor = [UIColor grayColor];
-    shadow.shadowOffset = CGSizeMake(1, 1);
-    
-    [title addAttributes:@ {
-                            NSStrokeWidthAttributeName  : @2.0f,
-                            NSFontAttributeName         : font,
-                            NSStrokeColorAttributeName  : [UIColor redColor],
-                            NSShadowAttributeName       : shadow
-                            }
-                   range:rangeOfTitle];
+//    NSShadow *shadow = [NSShadow new];
+//    shadow.shadowBlurRadius = 2.0f;
+//    shadow.shadowColor = [UIColor grayColor];
+//    shadow.shadowOffset = CGSizeMake(1, 1);
+//    
+//    [title addAttributes:@{
+//                           NSStrokeWidthAttributeName  : @2.0f,
+//                           NSFontAttributeName         : font,
+//                           NSStrokeColorAttributeName  : [UIColor redColor],
+//                           NSShadowAttributeName       : shadow
+//                           }
+//                   range:rangeOfTitle];
 
-    
     self.label.attributedText = title;
+}
+
+- (NSDictionary *)titleAttributes
+{
+    if (_titleAttributes == nil) {
+        UIFont *font = [UIFont fontWithName:@"DamascusBold" size:48.0f];
+
+        NSShadow *shadow = [NSShadow new];
+        shadow.shadowBlurRadius = 2.0f;
+        shadow.shadowColor = [UIColor grayColor];
+        shadow.shadowOffset = CGSizeMake(1, 1);
+        
+        _titleAttributes = @{
+                             NSStrokeWidthAttributeName  : @2.0f,
+                             NSFontAttributeName         : font,
+                             NSStrokeColorAttributeName  : [UIColor redColor],
+                             NSShadowAttributeName       : shadow
+                             };
+    }
+    
+    return _titleAttributes;
 }
 
 
